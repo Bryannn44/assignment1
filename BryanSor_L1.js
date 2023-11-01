e746f23ed628263149fc7556046b74db0467839a
Bryannn44 <023brysor@gmail.com>
2023-11-01 20:59:35 +0800

    changes(1)

--- BryanSor_L1.js
+++ BryanSor_L1.js
@@ -1,4 +1,6 @@
 module.exports = {
+    accounts : [1, 6, 10, 9, 43],
+    
     // #1 Convert SG to MY 
     convertToMY(a) {
         return a * 3.48;
@@ -13,9 +15,32 @@ module.exports = {
             return 3.48;
         }
         else if(a == "Euro"){
-            return 0.69
+            return 0.69;
+        }
+    },
+
+    //#3 Deposit cash
+    depositCash(a, i) {
+        if (a > 0) {
+          this.accounts[i] += a;
+          return "You just deposited $" + a + " and your total is $" + this.accounts[i];
+        } else {
+          return "Invalid amount";
+        }
+      },
+
+    //#4 Withdraw Cash
+    withdrawCash(a){
+        balance = 10
+
+        if(a < balance){
+            balance = balance - a;
+            return "You have withdraw $" + a + " and is left with $" + balance + " in the bank"; 
+        }
+        else{
+            return "You dont have that much money";
         }
     }
 
-    
+
 }
--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const BryanSor_L1 = require("./BryanSor_L1.js");
 
+
 console.log(BryanSor_L1.convertToMY(2));
 
-console.log (BryanSor_L1.checkCurrRate("US"))
+console.log (BryanSor_L1.checkCurrRate("US"));
+
+console.log(BryanSor_L1.depositCash(10, 4));
+
+console.log(BryanSor_L1.withdrawCash(9));
